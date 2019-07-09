afe78e3fb572502e450df29589577b9a8047d310
repoog <repoog@gmail.com>
2019-07-10 00:57:33 +0800

    update: add profile path

--- loveyou.sh
+++ loveyou.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
+is_start_path="/home/ubuntu/loveyou3000/is_start.txt"
+days_path="/home/ubuntu/loveyou3000/days.txt"
+ascii_path="/home/ubuntu/loveyou3000/ascii.txt"
+
 today=$[$[$(date +%w)+1]%7]
-is_start=$(cat is_start.txt)
+is_start=$(cat $is_start_path)
 
 if [ $today -eq 1 -a $is_start -eq 0 ]; then
-	echo 1 > is_start.txt
+	echo 1 > $is_start_path
 elif [ $is_start -eq 0 ]; then
 	echo "Wrong date, quit."
 	exit
 fi
 
-day=$[$(cat days.txt)+1]
-echo $[$day+1] > days.txt
+day=$[$(cat $days_path)+1]
+echo $[$day+1] > $days_path
 
 line_num=$[$[$day/7]+1]
 ascii_num=$[$day%7]
 
-line_ascii=$(sed -n $[$line_num]p ascii.txt)
+line_ascii=$(sed -n $[$line_num]p $ascii_path)
 flag=${line_ascii:$[$ascii_num-1]:1}
 
 if [ $flag -eq 0 ]; then
